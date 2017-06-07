9dc7706cad59b41017845d5c4986b95b698726d4
chris ricketts <chris.ricketts@steribar.com>
2017-06-07 16:22:54 +0100

    Improved demo styling and linked from README

--- theresamay.js
+++ theresamay.js
@@ -145,5 +145,5 @@ theresaMay.prototype.askQuestion = function (question) {
         responseArray[i] = responseItem;
     }
 
-    return responseArray.join(". ", );
+    return responseArray.join(". ");
 }
